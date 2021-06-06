7de49bda9811a4858b22828f573266ec3785c314
Stephen Pozzuoli <stephenpozzuoli@gmail.com>
2021-06-06 11:46:25 -0400

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -193,3 +193,17 @@ INNER JOIN dept_emp AS de
 ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO sales_dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = ('Sales');
+
+SELECT * FROM sales_dept_info
